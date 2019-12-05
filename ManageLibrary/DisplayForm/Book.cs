bdf7d70fedd2f03debb499676987a22dfa8d3531
mizeks98 <tranquanghuu322@gmail.com>
2019-12-06 01:10:29 +0700

    add new source
    + add new source ver 1.0.0

---
+++ ManageLibrary/DisplayForm/Book.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace ManageLibrary.DisplayForm
+{
+    public partial class Book : DevExpress.XtraEditors.XtraForm
+    {
+        public Book()
+        {
+            InitializeComponent();
+        }
+
+        private void groupControl2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void tablePanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void groupControl1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
